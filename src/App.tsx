1c2f1e6825e6b37666302bbb8017e8b5d33c6651
Abbas <meerabbas110@gmail.com>
2023-07-21 16:51:50 +0530

    Color switch mode added toggle

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,16 @@ function App() {
     <Grid
       templateAreas={{
         base: `'nav' 'main'`,
-        lg: `'nav nav' 'main' 'aside'`,
+        lg: `'nav nav' 'aside main'`,
       }}
     >
       <GridItem area='nav'>
         <NavBar />
       </GridItem>
       <Show above='lg'>
-        <GridItem area='aside' bg='gold'>
-          Aside
-        </GridItem>
+        <GridItem area='aside'>Aside</GridItem>
       </Show>
-      <GridItem area='main' bg='blue'>
-        main
-      </GridItem>
+      <GridItem area='main'>main</GridItem>
     </Grid>
   );
 }
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,12 @@
-import { HStack, Image, Text } from '@chakra-ui/react';
+import { HStack, Image } from '@chakra-ui/react';
 import logo from '../assets/../assets/logo-removebg-preview.png';
+import ColorModeSwitch from './ColorModeSwitch';
 
 const NavBar = () => {
   return (
-    <HStack>
+    <HStack justifyContent='space-between' padding='10px'>
       <Image src={logo} boxSize='60px' />
-      <Text>NavBar</Text>
+      <ColorModeSwitch />
     </HStack>
   );
 };
