de092242570ab2a4102b7237b63e93eaefd8c90f
Abbas <meerabbas110@gmail.com>
2023-07-25 18:27:42 +0530

    implement searching

--- src/Hooks/UseGames.ts
+++ src/Hooks/UseGames.ts
@@ -22,7 +22,8 @@ useData<Game>('/games',
 {params:{
   genres: gameQuery.genre?.id, 
   platforms: gameQuery.platform?.id,
-  ordering: gameQuery.sortOrder
+  ordering: gameQuery.sortOrder,
+  search: gameQuery.searchText
 }}, 
 [gameQuery])
 export default UseGames
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,11 +3,15 @@ import logo from '../assets/../assets/logo-removebg-preview.png';
 import ColorModeSwitch from './ColorModeSwitch';
 import SearchInput from './SearchInput';
 
-const NavBar = () => {
+interface Props {
+  onSearch: (searchText: string) => void;
+}
+
+const NavBar = ({ onSearch }: Props) => {
   return (
     <HStack padding='10px'>
       <Image src={logo} boxSize='80px' />
-      <SearchInput />
+      <SearchInput onSearch={onSearch} />
       <ColorModeSwitch />
     </HStack>
   );
